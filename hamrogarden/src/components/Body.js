d77ac48c009c12242047cce873458d2d58277c8d
Sandeep Chaudhary <60634936+sandeepchy@users.noreply.github.com>
2022-05-28 21:09:16 +0545

    Revert "product details frontend"
    This reverts commit 9c3988e5752b21b416a68df9b02ea90eb01bfb7c.

--- hamrogarden/src/components/Body.js
+++ hamrogarden/src/components/Body.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import ProductDetailScreen from '../screens/ProductDetailScreen';
-import ProductsScreen from '../screens/ProductsScreen';
-import Addproduct from './AdminDashboard/ProductDashboard/Addproduct';
-import Login from './Login/Login';
-import Register from './Login/Register';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Addproduct from "./AdminDashboard/ProductDashboard/Addproduct";
+import Login from "./Login/Login";
+import Register from "./Login/Register";
 
 const Body = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/products' element={<ProductsScreen />} />
-        <Route path='/products/:id' element={<ProductDetailScreen />} />
-
-        <Route path='/register' element={<Register />} />
-        <Route path='/addproduct' element={<Addproduct />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/addproduct" element={<Addproduct />} />
       </Routes>
     </>
   );
